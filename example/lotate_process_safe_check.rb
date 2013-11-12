e6d62b8b4b452f58443a611468dfdfb00501352c
SpringMT <today.is.sky.blue.sky@gmail.com>
2013-11-12 16:15:05 +0900

    mod exapmles

--- example/lotate_process_safe_check.rb
+++ example/lotate_process_safe_check.rb
@@ -3,27 +3,31 @@ $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'daily_logger'
 require 'parallel'
 require 'timecop'
+require 'test/unit'
+
 Timecop.scale(24 * 60 * 60)
 
-proc_num = 2
-execute_num = 10000
+$proc_num = 2
+$execute_num = 10000
 
 logger = DailyLogger.new("example/log/lotate_test_process")
-Parallel.map(['a', 'b'], :in_processes => proc_num) do |letter|
-  execute_num.times do
+Parallel.map(['a', 'b'], :in_processes => $proc_num) do |letter|
+  $execute_num.times do
   logger.info letter * 5000
   end
 end
-total_num = `wc -l example/log/lotate_test_process*`
-p total_num
-p "expected total line num #{execute_num * proc_num}"
-#p "executed total line num #{}"
 
-=begin
-% ruby example/lotate_process_safe_check.rb
-no warn!!!
+$total_num = `wc -l example/log/lotate_test_process*`.split("\n").map(&:strip).grep(/\stotal\z/).first.split(' ').first.to_i
+p "Expected total line num #{$execute_num * $proc_num}"
+p "Actually total line num #{$total_num}"
 
-% ls -al example/log                                                                           ~/daily_logger
-total 196232
-=end
+
+class DailyLoggerTC < Test::Unit::TestCase
+  def test_logger
+    assert_equal($execute_num * $proc_num, $total_num)
+  end
+  def teardown
+    p 'rm -rf example/log/*'
+  end
+end
 
--- example/lotate_thread_safe_check.rb
+++ example/lotate_thread_safe_check.rb
@@ -3,23 +3,31 @@ $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'daily_logger'
 require 'parallel'
 require 'timecop'
+require 'test/unit'
+
 Timecop.scale(24 * 60 * 60)
 
-logger = DailyLogger.new("example/log/lotate_test")
-Parallel.map(['a', 'b'], :in_threads => 2) do |letter|
-  10000.times do
+$proc_num = 2
+$execute_num = 10000
+
+logger = DailyLogger.new("example/log/lotate_test_thread")
+Parallel.map(['a', 'b'], :in_threads => $proc_num) do |letter|
+  $execute_num.times do
   logger.info letter * 5000
   end
 end
 
-=begin
-BEFORE
-% ruby example/lotate_thread_safe_check.rb                                                     ~/daily_logger
-/Users/hoge/daily_logger/lib/daily_logger/adapter/file.rb:23:in `write': stream closed (IOError)
+$total_num = `wc -l example/log/lotate_test_thread*`.split("\n").map(&:strip).grep(/\stotal\z/).first.split(' ').first.to_i
+p "Expected total line num #{$execute_num * $proc_num}"
+p "Actually total line num #{$total_num}"
 
-AFTER
-% ruby example/lotate_thread_safe_check.rb
-no warn!!!
-% ls -al example/log/*                                                                         ~/daily_logger
-=end
+class DailyLoggerTC < Test::Unit::TestCase
+  def test_logger
+    assert_equal($execute_num * $proc_num, $total_num)
+  end
+  def teardown
+    p 'rm -rf example/log/*'
+  end
+end
+
 
